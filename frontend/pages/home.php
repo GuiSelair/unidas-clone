48c72ce415c5791f18dbdd835cac0b2452bcaac6
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-08-08 00:37:46 -0300

    Initialize project 🚀

---
+++ frontend/pages/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Unidos - Aluguel de carros</title>
+	<link rel="stylesheet" href="../styles/pages/home.css">
+	<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+</head>
+<body>
+	<header>
+		<div>
+			<img src="../assets/logo.png" alt="Unidos - Aluguel de carros" />
+			<div class="authenticate-buttons">
+				<button class="login-button">
+					<i 
+						class="iconify" 
+						data-inline="false" 
+						data-icon="ant-design:user-outlined" ></i>
+						<span>LOGIN</span>
+				</button>
+				<button class="register-button">
+					<span>CADASTRE-SE</span>
+					<i 
+						class="iconify" 
+						data-inline="false" 
+						data-icon="bi:arrow-right-circle-fill" ></i>
+				</button>
+			</div>
+		</div>
+	</header>
+
+</body>
+</html>
