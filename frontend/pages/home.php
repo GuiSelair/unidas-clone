3a72c6678aa9fb7e6427a14991612b417f959e16
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-08-16 22:31:12 -0300

    Feat: Added header datepicker

--- frontend/pages/home.php
+++ frontend/pages/home.php
@@ -9,11 +9,21 @@
 	<link rel="stylesheet" href="./frontend/components/Header/styles.css">
 	<link rel="stylesheet" href="./frontend/styles/pages/home.css">
 	<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
 </head>
 <body>
 	<?php include "./frontend/components/Header/index.php" ?>
-	<section>
-		<h3>Unidos - Aluguel de carros</h3>
+	<section class="datePickerWrapper">
+		<div>
+			<h3>Unidos - Aluguel de carros</h3>
+			<div>
+
+			</div>
+		</div>
 	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
