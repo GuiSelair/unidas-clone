5b0ff9b7bb22038760e9b2852e2b47e0cde03931
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-08-18 22:17:58 -0300

    Feat: Added SeachAvailableVehiclesForm and Controller

--- frontend/pages/home.php
+++ frontend/pages/home.php
@@ -7,6 +7,7 @@
 	<title>Unidos - Aluguel de carros</title>
 	<link rel="stylesheet" href="./frontend/styles/global.css">
 	<link rel="stylesheet" href="./frontend/components/Header/styles.css">
+	<link rel="stylesheet" href="./frontend/components/Forms/SearchAvailableVehiclesForm/styles.css">
 	<link rel="stylesheet" href="./frontend/styles/pages/home.css">
 	<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
@@ -17,13 +18,13 @@
 		<div>
 			<h3>Unidos - Aluguel de carros</h3>
 			<div>
-
+				<?php include "./frontend/components/Forms/SearchAvailableVehiclesForm/index.php" ?>
 			</div>
 		</div>
 	</section>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
 </body>
 </html>
